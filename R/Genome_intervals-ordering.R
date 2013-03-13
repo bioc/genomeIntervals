605a2194c5b4bcaf2e2bccba5767231cf69d35fd
Julien Gagneur <gagneur@embl.de>
2013-03-13 08:15:57 +0000

    Genome_intervals-ordering added
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genomeIntervals@74266 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/Genome_intervals-ordering.R
@@ -0,0 +1,48 @@
+### sorting ranking and ordering
+
+## xtfrm() is a generic auxiliary function that produces a numeric vector which
+##  will sort in the same order as 'x'.
+## xtfrm() orders Genome_intervals objects according to 
+## 1) seq_name
+## 2) [start & !inter-base < [start & inter-base < (start & !inter-base < (start & inter-base
+## 3) stop) & !inter-base < stop) & inter-base < stop] & !inter-base < stop] & inter-base 
+## 4) strand (for Genome_intervals_stranded object)
+##
+## We get automatically order() which calls xtfrm() and implements rank() and sort() based on order() (code was taken from IRanges)
+
+setMethod("xtfrm", "Genome_intervals", function(x){
+			a = x[,1] + 1*(!closed(x)[,1]) + 0.5*inter_base(x)
+			b = x[,2] - 1*(!closed(x)[,2]) + 0.5*inter_base(x)
+			o = order(seq_name(x), a, b)
+			rv = integer(length(o))
+			rv[o] = seq_len(length(o))
+			rv
+		}
+)
+
+setMethod("xtfrm", "Genome_intervals_stranded", function(x){
+			a = x[,1] + 1*(!closed(x)[,1]) + 0.5*inter_base(x)
+			b = x[,2] - 1*(!closed(x)[,2]) + 0.5*inter_base(x)
+			o = order(seq_name(x), a, b, strand(x))
+			rv = integer(length(o))
+			rv[o] = seq_len(length(o))
+			rv
+		}
+)
+
+### sort() relies on order() and on a "[" method for 'x'.
+setMethod("sort", "Genome_intervals",
+		function(x, decreasing=FALSE, ...) 
+		{
+			x[order(x, decreasing=decreasing)]
+		}
+)
+
+setMethod("rank", "Genome_intervals",
+		function(x, na.last=TRUE, ties.method=c("average", "first", "random", "max", "min"))
+		{
+			if (!missing(ties.method) && !identical(ties.method, "first"))
+				stop("only 'ties.method=\"first\"' is supported when ranking GenomeIntervals")
+			xtfrm(x)
+		}
+)
